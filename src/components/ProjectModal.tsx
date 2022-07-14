4291875d15b9cefca92363d44b3d18960088c674
narrwep27 <narrwep@gmail.com>
2022-07-14 15:56:11 -0400

    primary files created

---
+++ src/components/ProjectModal.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ProjectModal = () => {
+    return (
+        <div>
+            <h1>Project Modal</h1>
+        </div>
+    );
+};
+
+export default ProjectModal;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../styles/Home.css';
+
+const Home = () => {
+    return (
+        <div>
+            <header className="home-header">
+                <h1>Narrwe Park</h1>
+                <p>Full Stack Developer | Software Engineer | Educator</p>
+            </header>
+        </div>
+    );
+};
+
+export default Home;
